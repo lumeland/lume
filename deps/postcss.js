aab0cc0c7788523bf05974232576b1c4170062ad
Oscar Otero <oom@oscarotero.com>
2020-09-12 16:37:44 +0200

    removed uncompatible postcss plugins

--- deps/postcss.js
+++ deps/postcss.js
@@ -1,11 +1,7 @@
 import postcss from "https://dev.jspm.io/postcss";
-import postcssImport from "https://dev.jspm.io/postcss-import";
-import postcssUrl from "https://dev.jspm.io/postcss-url";
 import postcssPresetEnv from "https://dev.jspm.io/postcss-preset-env";
 
 const processor = postcss([
-  postcssImport(),
-  postcssUrl(),
   postcssPresetEnv({
     stage: 1,
     features: {
