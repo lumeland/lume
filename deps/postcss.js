96465a71d2d9972878725ace81e646eca2190d3b
Oscar Otero <oom@oscarotero.com>
2020-10-20 22:39:13 +0200

    updated postcss

--- deps/postcss.js
+++ deps/postcss.js
@@ -1,2 +1,2 @@
-export { default as postcss } from "https://raw.githubusercontent.com/oscarotero/postcss-deno/master/lib/mod.js";
+export { default as postcss } from "https://deno.land/x/postcss@8.1.2/mod.js";
 export { default as postcssPresetEnv } from "https://dev.jspm.io/postcss-preset-env";
