6b6f52f9e04754fe2de002ee0acaaba3eeb406a8
Oscar Otero <oom@oscarotero.com>
2020-11-20 18:51:55 +0100

    updated version number

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ if (import.meta.main) {
 }
 
 export default async function cli(args) {
-  const version = "v0.9.3";
+  const version = "v0.9.4";
   let stop = false;
   const options = parse(args, {
     boolean: ["serve", "init", "version", "dev", "help", "upgrade"],
