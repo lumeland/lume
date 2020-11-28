575a9238a0f558b973e7433c7755fabf4736b0b0
Oscar Otero <oom@oscarotero.com>
2020-11-28 02:12:35 +0100

    fix multicommands scripts #7

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 import { brightGreen } from "./deps/colors.js";
+import { join } from "./deps/path.js";
 
 export default class Scripts {
   scripts = new Map();
@@ -7,13 +8,17 @@ export default class Scripts {
     this.site = site;
   }
 
-  set(name, ...scripts) {
-    this.scripts.set(name, scripts);
+  set(name, ...commands) {
+    this.scripts.set(name, parseCommands(commands));
   }
 
   async run(...names) {
+    const options = {
+      cwd: this.site.options.cwd,
+    };
+
     for (const name of names) {
-      const success = await this.#runScript(name);
+      const success = await this.#runScript(name, options);
 
       if (!success) {
         return false;
@@ -23,28 +28,51 @@ export default class Scripts {
     return true;
   }
 
-  async #runScript(name) {
+  async #runScript(name, options) {
     if (this.scripts.has(name)) {
       name = this.scripts.get(name);
       return this.run(...name);
     }
 
     if (Array.isArray(name)) {
-      const results = await Promise.all(name.map((n) => this.#runScript(n)));
+      const results = await Promise.all(
+        name.map((n) => this.#runScript(n, options)),
+      );
       return (results.every((success) => success)) ? 0 : 1;
     }
 
-    return this.#runCommand(name);
+    return this.#runCommand(name, options);
   }
 
-  async #runCommand(command) {
+  async #runCommand(command, options) {
     console.log(`⚡️ ${brightGreen(command)}`);
     const cmd = Array.from(command.matchAll(/('([^']*)'|"([^"]*)"|[\S]+)/g))
       .map((piece) => piece[2] || piece[1]);
 
-    const process = Deno.run({ cmd, cwd: this.site.options.cwd });
+    const process = Deno.run({ cmd, ...options });
     const status = await process.status();
     process.close();
+
+    if (cmd[0] === "cd") {
+      options.cwd = join(options.cwd, cmd[1]);
+    }
+
     return status.success;
   }
+}
+
+function parseCommands(commands, result = []) {
+  commands.forEach((command) => {
+    if (typeof command === "string") {
+      //Split the commands joined by " && " and " & "
+      const subcommands = command.split(/\s+&&\s+/)
+        .map((s) => s.includes(" & ") ? s.split(/\s+&\s+/) : s);
+
+      result.push(...subcommands);
+    } else {
+      result.push(parseCommands(command));
+    }
+  });
+
+  return result;
 }
