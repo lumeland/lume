dd7726e73850e151cc979558cc0c3799a76f5fae
Oscar Otero <oom@oscarotero.com>
2020-12-01 00:33:04 +0100

    removed failing dependency

--- deps/nunjuks.js
+++ deps/nunjuks.js
@@ -1,4 +1,2 @@
 // Using a fork of https://github.com/denjucks/denjucks
-
-// @deno-types="https://cdn.jsdelivr.net/gh/lumeland/denjucks@2.0.0/mod.d.js"
 export { default } from "https://cdn.jsdelivr.net/gh/lumeland/denjucks@2.0.0/mod.js";
