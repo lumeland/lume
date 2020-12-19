a095ee5b66b9e4d190974762479f088829ebc9a9
Oscar Otero <oom@oscarotero.com>
2020-12-19 01:20:55 +0100

    remove extra space next to frontmatter

--- loaders/yaml.js
+++ loaders/yaml.js
@@ -7,12 +7,21 @@ export default async function (path) {
 }
 
 export function parseFrontmatter(content) {
-  if (content.startsWith("---")) {
+  if (content.startsWith("---") && content.charAt(3) !== "-") {
     const end = content.indexOf("---", 3);
 
     if (end !== -1) {
       const data = parse(content.slice(3, end));
       data.content = content.slice(end + 3);
+
+      if (data.content[0] === "\r") {
+        data.content = data.content.slice(1);
+      }
+
+      if (data.content[0] === "\n") {
+        data.content = data.content.slice(1);
+      }
+
       return data;
     }
   }
