dfd6c49faf39bf507a83f2e43af84726a72e639f
ewired <37567272+ewired@users.noreply.github.com>
2020-12-31 22:17:21 -0600

    Allow custom PostCSS plugins in CSS plugin

--- plugins/css.js
+++ plugins/css.js
@@ -1,13 +1,14 @@
 import textLoader from "../loaders/text.js";
 import { postcss, postcssImport, postcssNesting } from "../deps/postcss.js";
 
-export default function () {
+export default function (plugins = []) {
   return (site) => {
     const runner = postcss([
       postcssImport({
         path: site.src("_includes"),
       }),
       postcssNesting(),
+      ...plugins,
     ]);
 
     site.loadAssets([".css"], textLoader);
