2382207dd7f17ca44238b36b553ad3e2c01edd1b
Oscar Otero <oom@oscarotero.com>
2021-03-14 13:18:23 +0100

    moved the code of search-compile.js to search.js

--- helpers/search.js
+++ helpers/search.js
@@ -1,5 +1,3 @@
-import { compileFilter, compileSort } from "./search-compiler.js";
-
 export default class Search {
   #site = null;
   #cache = null;
@@ -101,4 +99,63 @@ function buildFilter(query) {
   });
 
   return compileFilter(filter);
+}
+
+function compileFilter(query) {
+  const filters = [];
+  const args = [];
+
+  Object.keys(query).forEach((key, index) => {
+    const value = `value${index}`;
+    filters.push(compileCondition(key, value));
+    args.push(value);
+  });
+
+  args.push(`return (page) => ${filters.join(" && ")};`);
+
+  const factory = new Function(...args);
+
+  return factory(...Object.values(query));
+}
+
+function compileSort(path) {
+  path = path.replaceAll(".", "?.");
+
+  return new Function("a", "b", `return (a.${path} < b.${path}) ? -1 : 1`);
+}
+
+function compileCondition(key, value) {
+  let [path, operator] = key.trim().split(" ");
+
+  path = path.replaceAll(".", "?.");
+
+  if (!operator || operator === "=") {
+    return `page.${path} === ${value}`;
+  }
+
+  if (operator === "!=") {
+    return `page.${path} !== ${value}`;
+  }
+
+  if (operator === "^=") {
+    return `page.${path}?.startsWith(${value})`;
+  }
+
+  if (operator === "$=") {
+    return `page.${path}?.endsWith(${value})`;
+  }
+
+  if (operator === "~=") {
+    return `page.${path}?.includes(${value})`;
+  }
+
+  if (operator === "ALL") {
+    return `${value}.every((i) => page.${path}?.includes(i))`;
+  }
+
+  if (operator === "SOME") {
+    return `${value}.some((i) => page.${path}?.includes(i))`;
+  }
+
+  throw new Error(`Invalid conditional operator ${operator}`);
 }
