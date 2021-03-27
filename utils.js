dc92a516079eb005a91a6396e9bfcca179338cf1
Oscar Otero <oom@oscarotero.com>
2021-03-27 15:09:33 +0100

    windows bugfix

--- utils.js
+++ utils.js
@@ -74,7 +74,7 @@ export function merge(defaults, user) {
 
 export function normalizePath(path) {
   if (SEP !== "/") {
-    return path.replaceAll(SEP, "/");
+    path = path.replaceAll(SEP, "/");
   }
 
   if (path === ".") {
