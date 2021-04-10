08c08d276dc3a366ad1219c333b526a3ab490afa
Valtteri Laitinen <dev@valtlai.fi>
2021-04-10 17:12:11 +0300

    Update rest of dependencies
    This also adds the missing version numbers.

--- deps/react.js
+++ deps/react.js
@@ -1,4 +1,4 @@
-import React from "https://jspm.dev/react";
-import ReactDOMServer from "https://jspm.dev/react-dom/server";
+import React from "https://jspm.dev/react@17.0.2";
+import ReactDOMServer from "https://jspm.dev/react-dom@17.0.2/server";
 
 export { React, ReactDOMServer };
