5c1b169d06e9fa87c6e3afff6b49fa99d08751d8
Valtteri Laitinen <dev@valtlai.fi>
2021-04-27 18:06:47 +0300

    Update postcss-nesting

--- deps/postcss.js
+++ deps/postcss.js
@@ -1,3 +1,3 @@
 export { default as postcss } from "https://deno.land/x/postcss@8.2.13/mod.js";
 export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.2/mod.js";
-export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/lumeland/postcss-nesting@v0.1.0/mod.js";
+export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/lumeland/postcss-nesting@v0.2.0/mod.js";
