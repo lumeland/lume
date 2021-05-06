lumeland/lume

commit fab476770c23278b2913b07ffd654e5ac475e82b

Improve slugifier
* Fix the removal of accents: the accented letters were removed altogether instead of just their accents.
* Uses the separator more. For example, “200,000*7” slugifies to “200-000-7” instead of “2000007”.
* Uses the replacements also in non-ASCII alphanumeric mode.
* Reordered the options so that the booleans are grouped together.

utils.js
@@ -88,25 +88,27 @@ export function normalizePath(path) {
 
 export function slugify(
   string,
-  { lowercase, separator, replace, alphanumeric },
+  { lowercase, alphanumeric, separator, replace },
 ) {
   if (lowercase) {
     string = string.toLowerCase();
   }
 
-  string = string.replaceAll(/[\s_-]+/g, separator);
-
-  if (!alphanumeric) {
-    return string;
+  if (alphanumeric) {
+    string = string.normalize("NFKD");
   }
 
-  return string.replaceAll(/[^\w\/.-]/gu, (char) => {
+  string = string.replaceAll(/[^a-z\d\/.-]/gu, (char) => {
     if (char in replace) {
       return replace[char];
     }
-    char = char.normalize("NFKD");
-    return /^\w+$/.test(char) ? char : "";
+
+    char = /[\p{L}\u0300-\u036F]/u.test(char) ? char : "-";
+
+    return alphanumeric && char !== "-" ? "" : char;
   });
+
+  return string.replaceAll(/-+/g, separator);
 }
 
 export function searchByExtension(path, extensions) {

site.js
@@ -20,8 +20,8 @@ const defaults = {
   prettyUrls: true,
   slugifyUrls: {
     lowercase: true,
-    separator: "-",
     alphanumeric: true,
+    separator: "-",
     replace: {
       "ð": "d",
       "ø": "o",

commit 9b2b2e771495ef879755ba3cf1d6228077ab722c

Fix slugifier
* Allow replacing accented letters in alphanumeric mode, like `{ "ä": "ae" }`. This was broken in fab476770c23278b2913b07ffd654e5ac475e82b.
* Remove all the leading or trailing separators instead of only the first.

utils.js
@@ -94,22 +94,22 @@ export function slugify(
     string = string.toLowerCase();
   }
 
-  if (alphanumeric) {
-    string = string.normalize("NFKD");
-  }
-
   string = string.replaceAll(/[^a-z\d\/.-]/giu, (char) => {
     if (char in replace) {
       return replace[char];
     }
 
+    if (alphanumeric) {
+      char = char.normalize("NFKD").replaceAll(/[\u0300-\u036F]/g, "");
+    }
+
     char = /[\p{L}\u0300-\u036F]/u.test(char) ? char : "-";
 
-    return alphanumeric && char !== "-" ? "" : char;
+    return alphanumeric && /[^\w-]/.test(char) ? "" : char;
   });
 
   return string
-    .replaceAll(/(?<=^|\/)-|-(?=\/|$)/g, "")
+    .replaceAll(/(?<=^|\/)-+|-+(?=$|\/)/g, "")
     .replaceAll(/-+/g, separator);
 }
 