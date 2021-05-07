e4c1268bfe407cc65e70dd6a52f559de23a6fa2b
Oscar Otero <oom@oscarotero.com>
2021-05-07 17:58:52 +0200

    updated postcss_import plugin

--- deps/postcss.js
+++ deps/postcss.js
@@ -1,3 +1,3 @@
 export { default as postcss } from "https://deno.land/x/postcss@8.2.14/mod.js";
-export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.2/mod.js";
+export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.3/mod.js";
 export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/lumeland/postcss-nesting@v0.2.0/mod.js";
