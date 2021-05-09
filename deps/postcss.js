da5562126f44c3f3c0d892aaa8955d970bb1f12f
Oscar Otero <oom@oscarotero.com>
2021-05-09 14:19:59 +0200

    updated autoprefixer

--- deps/postcss.js
+++ deps/postcss.js
@@ -1,4 +1,4 @@
 export { default as postcss } from "https://deno.land/x/postcss@8.2.14/mod.js";
 export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.3/mod.js";
 export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/lumeland/postcss-nesting@v0.2.0/mod.js";
-export { default as autoprefixer } from "https://raw.githubusercontent.com/lumeland/autoprefixer-deno/master/deno/mod.js";
+export { default as autoprefixer } from "https://deno.land/x/postcss_autoprefixer@0.1.0/mod.js";
