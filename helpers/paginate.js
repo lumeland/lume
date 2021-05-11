771a3839f2932a8ee0bf5b92c274486024b2fb04
Oscar Otero <oom@oscarotero.com>
2021-05-11 22:50:42 +0200

    paginate must use the prettyUrls config as default

--- helpers/paginate.js
+++ helpers/paginate.js
@@ -1,41 +1,45 @@
-const defaults = {
-  size: 10,
-  url: (page) => `./page-${page}`,
-};
+export default function (site) {
+  const ext = site.options.prettyUrls ? "/index.html" : ".html";
 
-export default function* paginate(results, options = {}) {
-  options = { ...defaults, ...options };
+  const defaults = {
+    size: 10,
+    url: (page) => `./page-${page}${ext}`,
+  };
 
-  const totalResults = results.length;
-  const totalPages = Math.ceil(results.length / options.size);
-  let page = 1;
-  let data = createPageData(page);
+  return function* paginate(results, options = {}) {
+    options = { ...defaults, ...options };
 
-  for (const result of results) {
-    data.results.push(result);
+    const totalResults = results.length;
+    const totalPages = Math.ceil(results.length / options.size);
+    let page = 1;
+    let data = createPageData(page);
 
-    if (data.results.length >= options.size) {
-      yield data;
+    for (const result of results) {
+      data.results.push(result);
 
-      data = createPageData(++page);
+      if (data.results.length >= options.size) {
+        yield data;
+
+        data = createPageData(++page);
+      }
     }
-  }
 
-  if (data.results.length) {
-    yield data;
-  }
+    if (data.results.length) {
+      yield data;
+    }
 
-  function createPageData(page) {
-    return {
-      url: options.url(page),
-      results: [],
-      pagination: {
-        page,
-        totalPages,
-        totalResults,
-        previous: page > 1 ? options.url(page - 1) : null,
-        next: totalPages > page ? options.url(page + 1) : null,
-      },
-    };
-  }
+    function createPageData(page) {
+      return {
+        url: options.url(page),
+        results: [],
+        pagination: {
+          page,
+          totalPages,
+          totalResults,
+          previous: page > 1 ? options.url(page - 1) : null,
+          next: totalPages > page ? options.url(page + 1) : null,
+        },
+      };
+    }
+  };
 }
--- plugins/search.js
+++ plugins/search.js
@@ -3,7 +3,7 @@ import paginate from "../helpers/paginate.js";
 
 export default function () {
   return (site) => {
-    site.data("paginate", paginate);
+    site.data("paginate", paginate(site));
 
     const search = new Search(site);
     site.data("search", search);
