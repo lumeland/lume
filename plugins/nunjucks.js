e96f2e1f9266e9b3cf4ccc12d636aafbb28e186e
Oscar Otero <oom@oscarotero.com>
2021-06-25 18:24:05 +0200

    moved nunjucks setup to plugin (from engine)

--- plugins/nunjucks.js
+++ plugins/nunjucks.js
@@ -1,3 +1,4 @@
+import nunjucks from "../deps/nunjucks.js";
 import NunjucksEngine from "../engines/nunjucks.js";
 import loader from "../loaders/text.js";
 import { merge } from "../utils.js";
@@ -13,18 +14,37 @@ export default function (userOptions) {
   const options = merge(defaults, userOptions);
 
   return (site) => {
-    const nunjucksEngine = new NunjucksEngine(site, options.options);
+    // Create the nunjucks environment instance
+    const fsLoader = new nunjucks.FileSystemLoader(site.includes());
+    const engine = new nunjucks.Environment(fsLoader, options.options);
 
     for (const [name, fn] of Object.entries(options.plugins)) {
-      nunjucksEngine.engine.addExtension(name, fn);
+      engine.addExtension(name, fn);
     }
 
-    site.loadPages(options.extensions, loader, nunjucksEngine);
+    // Update cache
+    site.addEventListener("beforeUpdate", (ev) => {
+      for (const file of ev.files) {
+        const filename = site.src(file);
+        const name = loader.pathsToNames[filename];
+
+        if (name) {
+          delete loader.cache[name];
+          continue;
+        }
+      }
+    });
+
+    site.loadPages(
+      options.extensions,
+      loader,
+      new NunjucksEngine(site, engine),
+    );
     site.filter("njk", filter, true);
 
     function filter(string, data = {}) {
       return new Promise((resolve, reject) => {
-        nunjucksEngine.engine.renderString(string, data, (err, result) => {
+        engine.renderString(string, data, (err, result) => {
           if (err) {
             reject(err);
           } else {
