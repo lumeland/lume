23538531ec70c6f7fcf0fd11fbe0481795b84deb
Oscar Otero <oom@oscarotero.com>
2021-07-07 21:16:30 +0200

    removed import_map aliases to javascript files

--- ci.ts
+++ ci.ts
@@ -6,21 +6,6 @@ const baseUrl = new URL(".", import.meta.url).href;
 const imports = {
   "lume": join(baseUrl, "/mod.ts"),
   "lume/": join(baseUrl, "/"),
-  "lume/mod.js": join(baseUrl, "/mod.ts"),
-  "lume/plugins/attributes.js": join(baseUrl, "/plugins/attributes.ts"),
-  "lume/plugins/base_path.js": join(baseUrl, "/plugins/base_path.ts"),
-  "lume/plugins/bundler.js": join(baseUrl, "/plugins/bundler.ts"),
-  "lume/plugins/code_highlight.js": join(baseUrl, "/plugins/code_highlight.ts"),
-  "lume/plugins/date.js": join(baseUrl, "/plugins/date.ts"),
-  "lume/plugins/inline.js": join(baseUrl, "/plugins/inline.ts"),
-  "lume/plugins/jsx.js": join(baseUrl, "/plugins/jsx.ts"),
-  "lume/plugins/postcss.js": join(baseUrl, "/plugins/postcss.ts"),
-  "lume/plugins/pug.js": join(baseUrl, "/plugins/pug.ts"),
-  "lume/plugins/relative_urls.js": join(baseUrl, "/plugins/relative_urls.ts"),
-  "lume/plugins/slugify_urls.js": join(baseUrl, "/plugins/slugify_urls.ts"),
-  "lume/plugins/svg.js": join(baseUrl, "/plugins/svgo.ts"),
-  "lume/plugins/terser.js": join(baseUrl, "/plugins/terser.ts"),
-  "lume/plugins/eta.js": join(baseUrl, "/plugins/eta.ts"),
 };
 
 export const cli = join(baseUrl, "./cli/cli.ts");
