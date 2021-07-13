39cdc3247697e3dac81ade91dd482dc8110a0258
Valtteri Laitinen <dev@valtlai.fi>
2021-07-13 18:31:45 +0300

    Update std

--- deps/server.ts
+++ deps/server.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.100.0/http/server.ts";
+export * from "https://deno.land/std@0.101.0/http/server.ts";
--- deps/ws.ts
+++ deps/ws.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.100.0/ws/mod.ts";
+export * from "https://deno.land/std@0.101.0/ws/mod.ts";
