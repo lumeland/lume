5aa2a52543d662e8c497458ec94009a8da2462ac
Oscar Otero <oom@oscarotero.com>
2021-09-14 14:19:30 +0200

    test code_highlight plugin

---
+++ tests/assets/code_highlight/index.md
@@ -0,0 +1,15 @@
+# Code highlight plugin testing
+
+```html
+<p>This is a HTML code</p>
+```
+
+```
+Not highlighted code
+```
+
+```css
+p {
+  color: red
+}
+```
---
+++ tests/code_highlight.test.ts
@@ -0,0 +1,35 @@
+import { assertStrictEquals as equals } from "../deps/assert.ts";
+import { getSite, testPage } from "./utils.ts";
+import codeHighlight from "../plugins/code_highlight.ts";
+import { Page } from "../core.ts";
+
+Deno.test("code_hightlight plugin", async () => {
+  const site = getSite({
+    test: true,
+    src: "code_highlight",
+  });
+
+  site.use(codeHighlight());
+  await site.build();
+
+  testPage(site, "/index", (page) => {
+    equals(page.document?.querySelectorAll("pre code").length, 3);
+    equals(
+      getClass(page, "pre code.language-html"),
+      "language-html hljs language-xml",
+    );
+    equals(getClass(page, "pre + pre code"), null);
+    equals(getClass(page, "pre code.language-css"), "language-css hljs");
+  });
+
+  testPage(site, "/other", (page) => {
+    equals(getClass(page, "body > pre"), null);
+    equals(getClass(page, "body > code"), null);
+    equals(getClass(page, "body > pre > code"), null);
+    equals(getClass(page, "pre code.language-css"), "hljs language-css");
+  });
+});
+
+function getClass(page: Page, selector: string) {
+  return page.document?.querySelector(selector)?.getAttribute("class");
+}
