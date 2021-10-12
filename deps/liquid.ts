d5164d30c9dbdea734129474c99a02026a588370
Oscar Otero <oom@oscarotero.com>
2021-10-12 14:37:12 +0200

    added plugin for liquidjs template engine

--- deps/liquid.ts
+++ deps/liquid.ts
@@ -1,4 +1,1 @@
-// export * from "https://cdn.jsdelivr.net/gh/lumeland/liquidjs-deno@master/deno/liquid.ts";
-// export * from "https://cdn.jsdelivr.net/gh/lumeland/liquidjs-deno@master/deno/liquid-options.ts";
-export * from "../../../deno/liquidjs/deno/liquid.ts";
-export * from "../../../deno/liquidjs/deno/liquid-options.ts";
+export * from "https://cdn.jsdelivr.net/gh/lumeland/liquidjs-deno@master/deno/mod.ts";
--- plugins/liquid.ts
+++ plugins/liquid.ts
@@ -1,8 +1,14 @@
-import { Liquid as Liquidjs, LiquidOptions } from "../deps/liquid.ts";
-import LiquidEngine from "../core/engines/liquid.ts";
+import {
+  Context,
+  Liquid as Liquidjs,
+  LiquidOptions,
+  TagImplOptions,
+  TagToken,
+  Template,
+} from "../deps/liquid.ts";
 import loader from "../core/loaders/text.ts";
 import { merge } from "../core/utils.ts";
-import { Site } from "../core.ts";
+import { Data, Engine, Event, Helper, HelperOptions, Site } from "../core.ts";
 
 export interface Options {
   /** The list of extensions this plugin applies to */
@@ -22,7 +28,50 @@ const defaults: Options = {
   options: {},
 };
 
-/** A plugin to add support for Markdown */
+/** Template engine to render Liquid files */
+export class LiquidEngine implements Engine {
+  engine: Liquidjs;
+  cache: Map<string, Template[]> = new Map();
+
+  constructor(site: Site, engine: Liquidjs) {
+    this.engine = engine;
+
+    // Update the internal cache
+    site.addEventListener("beforeUpdate", (ev: Event) => {
+      for (const file of ev.files!) {
+        this.cache.delete(site.src(file));
+      }
+    });
+  }
+
+  async render(content: string, data: Data, filename: string) {
+    if (!this.cache.has(filename)) {
+      this.cache.set(
+        filename,
+        this.engine.parse(content, filename),
+      );
+    }
+    const template = this.cache.get(filename)!;
+    return await this.engine.render(template, data);
+  }
+
+  addHelper(name: string, fn: Helper, options: HelperOptions) {
+    switch (options.type) {
+      case "filter":
+        this.engine.registerFilter(name, fn);
+        break;
+
+      case "tag":
+        // Tag with body not supported yet
+        if (!options.body) {
+          this.engine.registerTag(name, createCustomTag(fn));
+        }
+        break;
+    }
+  }
+}
+
+/** Register the plugin to add support for Liquid files */
 export default function (userOptions?: Partial<Options>) {
   return function (site: Site) {
     const options = merge(
@@ -37,10 +86,28 @@ export default function (userOptions?: Partial<Options>) {
 
     const engine = new Liquidjs(liquidOptions);
 
+    // Load the liquid pages
     site.loadPages(
       options.extensions,
       loader,
       new LiquidEngine(site, engine),
     );
+  };
+}
+
+/**
+ * Create a custom tag
+ * https://liquidjs.com/tutorials/register-filters-tags.html#Register-Tags
+ */
+function createCustomTag(fn: Helper): TagImplOptions {
+  return {
+    parse(tagToken: TagToken) {
+      this.str = tagToken.args;
+    },
+
+    async render(ctx: Context) {
+      const str = await this.liquid.evalValue(this.str, ctx);
+      return fn(str);
+    },
   };
 }
