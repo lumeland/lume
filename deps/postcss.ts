d793b25e89e570116ca43a3025d6bb254f6ca960
Fernando Serboncini <fserb@fserb.com.br>
2021-11-16 21:10:37 -0500

    import SourceMapOptions

--- deps/postcss.ts
+++ deps/postcss.ts
@@ -2,12 +2,4 @@ export { default as postcss } from "https://deno.land/x/postcss@8.3.11/mod.js";
 export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.4/mod.js";
 export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/csstools/postcss-nesting@9.0.0/mod.js";
 export { default as autoprefixer } from "https://deno.land/x/postcss_autoprefixer@0.1.1/mod.js";
-
-export interface SourceMapOptions {
-  inline?: boolean
-  prev?: string | boolean | object | ((file: string) => string)
-  sourcesContent?: boolean
-  annotation?: string | boolean | ((file: string, root: any) => string)
-  from?: string
-  absolute?: boolean
-}
+export { SourceMapOptions } from "https://deno.land/x/postcss@8.3.11/lib/postcss.d.ts";
