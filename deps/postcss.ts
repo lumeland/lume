113a6aa73cf54a4a231b8cb92cc45a39c502f774
Fernando Serboncini <fserb@fserb.com.br>
2021-11-16 21:19:27 -0500

    postcss.d.ts

--- deps/postcss.ts
+++ deps/postcss.ts
@@ -2,4 +2,3 @@ export { default as postcss } from "https://deno.land/x/postcss@8.3.11/mod.js";
 export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.4/mod.js";
 export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/csstools/postcss-nesting@9.0.0/mod.js";
 export { default as autoprefixer } from "https://deno.land/x/postcss_autoprefixer@0.1.1/mod.js";
-export { SourceMapOptions } from "https://deno.land/x/postcss@8.3.11/lib/postcss.d.ts";
