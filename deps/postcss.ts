df133732478809cab3da6093bb754b0b133017d0
Oscar Otero <oom@oscarotero.com>
2021-11-18 19:16:15 +0100

    postcssNesting update

--- deps/postcss.ts
+++ deps/postcss.ts
@@ -1,5 +1,5 @@
 export { default as postcss } from "https://deno.land/x/postcss@8.3.11/mod.js";
 export { default as postcssImport } from "https://deno.land/x/postcss_import@0.1.4/mod.js";
-export { default as postcssNesting } from "https://cdn.jsdelivr.net/gh/csstools/postcss-nesting@9.0.0/mod.js";
+export { default as postcssNesting } from "https://cdn.jsdelivr.net/npm/postcss-nesting@10.0.2/mod.js";
 export { default as autoprefixer } from "https://deno.land/x/postcss_autoprefixer@0.1.1/mod.js";
 export type { SourceMapOptions } from "https://deno.land/x/postcss@8.3.11/lib/postcss.d.ts";
