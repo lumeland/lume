68d5fb35ea4e54c62a243a56d4e3be216daa5da8
Oscar Otero <oom@oscarotero.com>
2021-12-20 00:02:18 +0100

    fixed pug import

--- deps/pug.ts
+++ deps/pug.ts
@@ -1,2 +1,1 @@
-// export * from "https://cdn.jsdelivr.net/gh/lumeland/pug@v0.1.2/mod.ts";
-export * from "../../pug/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/lumeland/pug@v0.1.2/mod.ts";
