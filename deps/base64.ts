9af364210d64d38e2bd2bab27cec3ca89517147f
Oscar Otero <oom@oscarotero.com>
2022-04-18 19:59:23 +0200

    updated std and denosass

--- deps/base64.ts
+++ deps/base64.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.134.0/encoding/base64.ts";
+export * from "https://deno.land/std@0.135.0/encoding/base64.ts";
--- deps/colors.ts
+++ deps/colors.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.134.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.135.0/fmt/colors.ts";
