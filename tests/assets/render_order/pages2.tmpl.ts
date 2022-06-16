941178cd7ae86a6c47f798e3760e1512f4154430
Oscar Otero <oom@oscarotero.com>
2022-06-16 16:41:06 +0200

    fixed render_order test

--- tests/assets/render_order/pages2.tmpl.ts
+++ tests/assets/render_order/pages2.tmpl.ts
@@ -1,4 +1,4 @@
-export const renderOrder = 1;
+export const renderOrder = 2;
 
 export default function* () {
   const pages = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
--- tests/assets/render_order/pagination.tmpl.ts
+++ tests/assets/render_order/pagination.tmpl.ts
@@ -2,6 +2,7 @@ import { Paginator } from "../../../plugins/paginate.ts";
 import { Search } from "../../../plugins/search.ts";
 
 export const layout = "paginate.tmpl.js";
+export const renderOrder = 1;
 
 export default function* (
   { search, paginate }: { search: Search; paginate: Paginator },
