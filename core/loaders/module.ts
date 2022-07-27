70aa13a154d688048ff30212f2f437fdb62b799b
Oscar Otero <oom@oscarotero.com>
2022-07-27 19:13:24 +0200

    expose the module transformation for reusing

--- core/loaders/module.ts
+++ core/loaders/module.ts
@@ -6,6 +6,11 @@ import type { Data } from "../../core.ts";
 export default async function module(path: string): Promise<Data> {
   const url = isUrl(path) ? path : `file://${path}#${Date.now()}`;
   const mod = await import(url);
+  return toData(mod);
+}
+
+/** Transform the imported module to Data */
+export function toData(mod: Record<string, unknown>): Data {
   const data: Data = {};
 
   for (const [name, value] of Object.entries(mod)) {
