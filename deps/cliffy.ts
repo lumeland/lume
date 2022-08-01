7f3bb47d6aad6f1ad01e053c7fa89e3748dd4cab
Oscar Otero <oom@oscarotero.com>
2022-08-01 17:38:00 +0200

    updated deps

--- deps/cliffy.ts
+++ deps/cliffy.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/cliffy@v0.24.2/mod.ts";
+export * from "https://deno.land/x/cliffy@v0.24.3/mod.ts";
--- deps/path.ts
+++ deps/path.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.149.0/path/mod.ts";
+export * from "https://deno.land/std@0.150.0/path/mod.ts";
