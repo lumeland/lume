a89b8ffb27c5bf2f00511ab2a8e6177ec27cc1e5
Oscar Otero <oom@oscarotero.com>
2022-08-26 20:24:41 +0200

    improved pagination test

--- tests/pagination.test.ts
+++ tests/pagination.test.ts
@@ -8,10 +8,15 @@ Deno.test("pagination plugin function", () => {
   });
 
   const all = Array(90).fill(0).map((_, i) => i + 1);
-  const pages = [...paginator(all)];
+  const pages = paginator(all, {
+    each(data) {
+      data.title = `Page ${data.pagination.page}`;
+    },
+  });
 
   equals(pages.length, 9);
   equals(pages[0].url, "/page/1");
+  equals(pages[0].title, "Page 1");
   equals(pages[0].results.length, 10);
   equals(pages[0].results[9], 10);
   equals(pages[0].pagination.page, 1);
@@ -19,6 +24,7 @@ Deno.test("pagination plugin function", () => {
   equals(pages[0].pagination.totalResults, 90);
   equals(pages[0].pagination.previous, null);
   equals(pages[0].pagination.next, "/page/2");
+  equals(pages[8].title, "Page 9");
   equals(pages[8].pagination.previous, "/page/8");
   equals(pages[8].pagination.next, null);
 });
