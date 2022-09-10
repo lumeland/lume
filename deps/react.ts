1ee801e61fef77f5c2a2fc10e585f8f6a8825350
Oscar Otero <oom@oscarotero.com>
2022-09-10 19:56:41 +0200

    improve the react module exports

--- deps/react.ts
+++ deps/react.ts
@@ -1,4 +1,5 @@
 import React from "https://esm.sh/react@18.2.0";
 import ReactDOMServer from "https://esm.sh/react-dom@18.2.0/server";
 
+export * from "https://esm.sh/react@18.2.0";
 export { React, ReactDOMServer };
