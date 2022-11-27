4ba91444160942503d9805bc6468ee1b66884112
Oscar Otero <oom@oscarotero.com>
2022-11-27 11:42:48 +0100

    removed prism languages setting

--- plugins/prism.ts
+++ plugins/prism.ts
@@ -1,4 +1,4 @@
-import Prism, { loadLanguages } from "../deps/prism.ts";
+import Prism from "../deps/prism.ts";
 import { merge } from "../core/utils.ts";
 
 import type { Page, Site } from "../core.ts";
@@ -10,16 +10,12 @@ export interface Options {
 
   /** The css selector to apply prism */
   cssSelector: string;
-
-  /** The list of programing languages to load */
-  languages: string[];
 }
 
 // Default options
 export const defaults: Options = {
   extensions: [".html"],
   cssSelector: "pre code",
-  languages: [],
 };
 
 /** A plugin to syntax-highlight code using the prism library */
@@ -27,10 +23,6 @@ export default function (userOptions?: Partial<Options>) {
   const options = merge(defaults, userOptions);
 
   return (site: Site) => {
-    site.addEventListener(
-      "beforeBuild",
-      () => loadLanguages(options.languages),
-    );
     site.process(options.extensions, prism);
 
     function prism(page: Page) {
--- tests/prism.test.ts
+++ tests/prism.test.ts
@@ -1,14 +1,13 @@
 import { assertSiteSnapshot, build, getSite } from "./utils.ts";
 import prism from "../plugins/prism.ts";
+import "npm:prismjs@1.29.0/components/prism-less.js";
 
 Deno.test("Prism plugin", async (t) => {
   const site = getSite({
     src: "prism",
   });
 
-  site.use(prism({
-    languages: ["css", "less", "html", "js"],
-  }));
+  site.use(prism());
 
   await build(site);
   await assertSiteSnapshot(t, site);
