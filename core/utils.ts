6dbbecae8fed46edfc8bff07f739d4cb3d08560d
Oscar Otero <oom@oscarotero.com>
2022-12-15 11:01:29 +0100

    upgraded minimum Deno version supported

--- core/utils.ts
+++ core/utils.ts
@@ -58,7 +58,7 @@ export function log(...lines: (string | undefined)[]) {
 
 /** Check the compatibility with the current Deno version */
 export function checkDenoVersion(): void {
-  const minimum = "1.25.4";
+  const minimum = "1.29.0";
   const current = Deno.version.deno;
 
   if (current < minimum) {
