42a589edec2fcda5b7ef5522500508debb5c6894
Oscar Otero <oom@oscarotero.com>
2023-03-09 18:09:23 +0100

    updated deps

--- deps/preact.ts
+++ deps/preact.ts
@@ -1,3 +1,3 @@
-export * as preact from "npm:preact@10.13.0";
-export * as hooks from "npm:preact@10.13.0/hooks";
+export * as preact from "npm:preact@10.13.1";
+export * as hooks from "npm:preact@10.13.1/hooks";
 export { renderToString } from "npm:preact-render-to-string@5.2.6";
--- deps/terser.ts
+++ deps/terser.ts
@@ -1,2 +1,2 @@
-export { minify } from "npm:terser@5.16.5";
-export type { MinifyOptions } from "npm:terser@5.16.5";
+export { minify } from "npm:terser@5.16.6";
+export type { MinifyOptions } from "npm:terser@5.16.6";
