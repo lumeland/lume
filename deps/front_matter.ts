521fad56ca6924847513800ba67a962bdcf8813c
Oscar Otero <oom@oscarotero.com>
2023-03-25 13:33:27 +0100

    updated std urls

--- deps/front_matter.ts
+++ deps/front_matter.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.181.0/encoding/front_matter/yaml.ts";
+export * from "https://deno.land/std@0.181.0/front_matter/yaml.ts";
--- deps/yaml.ts
+++ deps/yaml.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.181.0/encoding/yaml.ts";
+export * from "https://deno.land/std@0.181.0/yaml/mod.ts";
