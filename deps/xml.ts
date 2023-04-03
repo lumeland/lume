773f54c854ed4374a9a43ef6e06268430db18481
adb-sh <git@adb.sh>
2023-04-03 21:34:23 +0200

    add RSS plugin

---
+++ deps/xml.ts
@@ -0,0 +1,1 @@
+export * from "https://deno.land/x/xml@2.1.0/mod.ts";
---
+++ plugins/rss.ts
@@ -0,0 +1,74 @@
+import { stringify } from "../deps/xml.ts";
+import { Page } from "../core/filesystem.ts";
+import { Search } from "../plugins/search.ts";
+
+import type { Data, Site } from "../core.ts";
+
+export interface Options {
+  filename: string;
+  query: string;
+  sort: string;
+  link: string;
+  title: string;
+}
+
+export const defaults: Options = {
+  filename: "/feed.rss",
+  query: "",
+  sort: "url=asc",
+  link: "http://127.0.0.1:3000/",
+  title: "My RSS Feed",
+};
+
+export default (userOptions?: Partial<Options>) => {
+  const options = {
+    ...defaults,
+    ...userOptions,
+  };
+
+  return (site: Site) => {
+    site.addEventListener("afterRender", () => {
+      const feed = Page.create(options.filename, getFeedContent(site));
+      site.pages.push(feed);
+    });
+
+    const getFeedContent = (site: Site) => {
+      const search = new Search(site, true);
+      const pages = search.pages(options.query, options.sort);
+      const items = pages.map((page: Data) => ({
+        title: page.title,
+        link: `${options.link}${page.url}`,
+        pubDate: page.date?.toISOString(),
+        description: page.excerpt,
+        content: {
+          "#text": page.content,
+        },
+      }));
+      const feed = {
+        xml: {
+          "@version": "1.0",
+          "@encoding": "UTF-8",
+        },
+        rss: {
+          "@xmlns:content": "http://purl.org/rss/1.0/modules/content/",
+          "@xmlns:wfw": "http://wellformedweb.org/CommentAPI/",
+          "@xmlns:dc": "http://purl.org/dc/elements/1.1/",
+          "@xmlns:atom": "http://www.w3.org/2005/Atom",
+          "@xmlns:sy": "http://purl.org/rss/1.0/modules/syndication/",
+          "@xmlns:slash": "http://purl.org/rss/1.0/modules/slash/",
+          "@version": "2.0",
+          channel: {
+            title: options.title,
+            link: options.link,
+            description: "",
+            lastBuildDate: new Date().toISOString(),
+            language: "us",
+            generator: "https://lume.land",
+            item: items,
+          },
+        },
+      };
+      return stringify(feed);
+    };
+  };
+};
