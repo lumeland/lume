2bfec967ed7d3feaf1eeb28dd556ec25b797b9a7
Oscar Otero <oom@oscarotero.com>
2023-04-06 12:35:25 +0200

    fixed multilanguage hreflang links

--- plugins/multilanguage.ts
+++ plugins/multilanguage.ts
@@ -168,7 +168,7 @@ export default function multilanguage(userOptions?: Partial<Options>): Plugin {
         const meta = document.createElement("link");
         meta.setAttribute("rel", "alternate");
         meta.setAttribute("hreflang", data.lang);
-        meta.setAttribute("href", data.url);
+        meta.setAttribute("href", site.url(data.url as string, true));
         document.head.appendChild(meta);
         document.head.appendChild(document.createTextNode("\n"));
       }
