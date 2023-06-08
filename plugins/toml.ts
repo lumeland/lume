e0b74cb768c29f1cbc910143d7663eb525bbcf7f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-08 12:39:33 +0800

    new toml plugin

---
+++ plugins/toml.ts
@@ -0,0 +1,30 @@
+import toml from "../core/loaders/yaml.ts";
+import { merge } from "../core/utils.ts";
+
+import type { Site } from "../core.ts";
+
+export interface Options {
+  /** The list of extensions this plugin applies to */
+  extensions: string[] | {
+    pages: string[];
+    data: string[];
+  };
+}
+
+// Default options
+export const defaults: Options = {
+  extensions: [".toml"],
+};
+
+/** A plugin to add support for YAML files */
+export default function (userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+  const extensions = Array.isArray(options.extensions)
+    ? { pages: options.extensions, data: options.extensions }
+    : options.extensions;
+
+  return (site: Site) => {
+    site.loadData(extensions.data, toml);
+    site.loadPages(extensions.pages, toml);
+  };
+}
