f6e4ba8daf31e9f5d28bdcab52c94449f09bea26
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-09 17:42:49 +0800

    add jsonc support for json plugin

--- core/loaders/json.ts
+++ core/loaders/json.ts
@@ -1,11 +1,12 @@
+import { parse } from "../../deps/jsonc.ts";
 import { isPlainObject, read } from "../utils.ts";
 
 import type { Data } from "../../core.ts";
 
-/** Load and parse a JSON file */
+/** Load and parse a JSON / JSONC file */
 export default async function json(path: string): Promise<Data> {
   const text = await read(path, false);
-  const content = JSON.parse(text);
+  const content = parse(text);
 
   if (!content) {
     return {};
