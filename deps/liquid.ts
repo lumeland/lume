61e4d1acb3ba6acb243c29be5c6fbbded3340a55
Martin Fischer <martin@push-f.com>
2023-06-30 09:57:16 +0200

    fix: make plugins/liquid.ts pass type check

--- deps/liquid.ts
+++ deps/liquid.ts
@@ -1,6 +1,7 @@
 export * from "npm:liquidjs@10.8.3";
-export type { LiquidOptions } from "npm:liquidjs@10.8.3/dist/liquid-options";
+export type { LiquidOptions } from "npm:liquidjs@10.8.3/dist/src/liquid-options";
 export type {
+  TagClass,
   TagImplOptions,
   Template,
-} from "npm:liquidjs@10.8.3/dist/template";
+} from "npm:liquidjs@10.8.3/dist/src/template";
