dd1be74d42e3d1954b64105930235a156f9a5003
Oscar Otero <oom@oscarotero.com>
2023-08-08 19:10:06 +0200

    updated deps, fixed vento components

--- deps/postcss.ts
+++ deps/postcss.ts
@@ -1,4 +1,4 @@
 export { default as postcss } from "npm:postcss@8.4.27";
 export { default as postcssImport } from "npm:postcss-import@15.1.0";
-export { default as postcssNesting } from "npm:postcss-nesting@12.0.0";
+export { default as postcssNesting } from "npm:postcss-nesting@12.0.1";
 export { default as autoprefixer } from "npm:autoprefixer@10.4.14";
