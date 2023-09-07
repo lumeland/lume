3f5b67c48d73afdf5e30c1ab0960cd1152269e95
Oscar Otero <oom@oscarotero.com>
2023-09-07 20:45:46 +0200

    comment postcss unocss plugin

--- deps/unocss.ts
+++ deps/unocss.ts
@@ -1,4 +1,4 @@
 export { createGenerator, type UserConfig } from "npm:@unocss/core@0.55.7";
 export { presetUno } from "npm:@unocss/preset-uno@0.55.7";
-export { default as postcssPlugin } from "npm:@unocss/postcss@0.55.7";
+// export { default as postcssPlugin } from "npm:@unocss/postcss@0.55.7";
 export const resetUrl = "https://unpkg.com/@unocss/reset@0.55.7";
