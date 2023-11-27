c645a1861953d240f080add25375e0ea867e630a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-27 11:59:28 +0800

    fix(tests/unocss): clean options

--- tests/unocss.test.ts
+++ tests/unocss.test.ts
@@ -1,8 +1,5 @@
 import { assertSiteSnapshot, build, getSite } from "./utils.ts";
 import unocss from "../plugins/unocss.ts";
-// https://github.com/denoland/deno/issues/19096
-import transformerVariantGroupImport from "npm:@unocss/transformer-variant-group@0.57.7";
-import transformerDirectivesImport from "npm:@unocss/transformer-directives@0.57.7";
 
 Deno.test("Unocss plugin", async (t) => {
   const site = getSite({
@@ -24,6 +21,7 @@ Deno.test("Unocss plugin (css file)", async (t) => {
 
   site.use(unocss({
     cssFile: "styles.css",
+    cssFileTransformers: [],
   }));
 
   await build(site);
@@ -31,22 +29,12 @@ Deno.test("Unocss plugin (css file)", async (t) => {
 });
 
 Deno.test("Unocss plugin (with transformers)", async (t) => {
-  // https://github.com/denoland/deno/issues/16458#issuecomment-1295003089
-  const transformerVariantGroup =
-    transformerVariantGroupImport as unknown as typeof transformerVariantGroupImport.default;
-  const transformerDirectives =
-    transformerDirectivesImport as unknown as typeof transformerDirectivesImport.default;
-
   const site = getSite({
     src: "unocss",
   });
 
   site.use(unocss({
     cssFile: "styles.css",
-    cssFileTransformers: [
-      transformerVariantGroup(),
-      transformerDirectives(),
-    ],
   }));
 
   await build(site);
