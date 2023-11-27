b18f870a6f75c87682ddc2f1e4b908f10b6358c4
Oscar Otero <oom@oscarotero.com>
2023-11-27 17:10:09 +0100

    updated deps

--- deps/http.ts
+++ deps/http.ts
@@ -1,4 +1,4 @@
-export { serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
-export { Server } from "https://deno.land/std@0.207.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.208.0/http/file_server.ts";
+export { Server } from "https://deno.land/std@0.208.0/http/server.ts";
 
-export type { ConnInfo } from "https://deno.land/std@0.207.0/http/server.ts";
+export type { ConnInfo } from "https://deno.land/std@0.208.0/http/server.ts";
--- deps/yaml.ts
+++ deps/yaml.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.207.0/yaml/mod.ts";
+export * from "https://deno.land/std@0.208.0/yaml/mod.ts";
