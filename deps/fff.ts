ae12c3b77d3b2ea56cbd5a6c56a677061451e1e4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-15 19:43:16 +0800

    new plugin fff

---
+++ deps/fff.ts
@@ -0,0 +1,13 @@
+export type {
+  FFFFlavoredFrontmatter
+} from "https://deno.land/x/fff@v1.1.0/src/types.ts";
+
+export {
+  type FFFTransformPreset,
+  transform,
+} from "https://deno.land/x/fff@v1.1.0/src/utils/transform.ts";
+
+export {
+  strict,
+  type StrictPresetOptions,
+} from "https://deno.land/x/fff@v1.1.0/src/utils/presets/strict.ts";
---
+++ plugins/fff.ts
@@ -0,0 +1,43 @@
+import { merge } from "../core/utils/object.ts";
+
+import {
+  type FFFFlavoredFrontmatter,
+  type FFFTransformPreset,
+  strict,
+  type StrictPresetOptions,
+  transform,
+} from "../deps/fff.ts";
+
+export interface Options {
+  presets: FFFTransformPreset[];
+  strict: false | StrictPresetOptions;
+}
+
+// Default options
+export const defaults: Options = {
+  presets: [],
+  strict: false,
+};
+
+export default function (userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+
+  return (site: Lume.Site) => {
+    site.preprocess([".html"], (pages) =>
+      pages.forEach((page) => {
+        page.data = transform(page.data, [
+          ...options.presets,
+          ...(options.strict === false ? [] : [strict(options.strict)]),
+        ]);
+      }));
+  };
+}
+
+/** Extends Data interface */
+declare global {
+  namespace Lume {
+    // deno-lint-ignore no-empty-interface
+    export interface Data
+      extends Omit<FFFFlavoredFrontmatter, "lang" | "tags"> {}
+  }
+}
