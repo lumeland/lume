b4f0825479539900809759551aad1cf8029a86cf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-20 22:09:27 +0800

    bump fff version

--- deps/fff.ts
+++ deps/fff.ts
@@ -1,13 +1,13 @@
 export type {
   FFFFlavoredFrontmatter,
-} from "https://deno.land/x/fff@v1.1.0/src/types.ts";
+} from "https://deno.land/x/fff@v1.2.0-alpha.0/src/types.ts";
 
 export {
   type FFFTransformPreset,
   transform,
-} from "https://deno.land/x/fff@v1.1.0/src/utils/transform.ts";
+} from "https://deno.land/x/fff@v1.2.0-alpha.0/src/utils/transform.ts";
 
 export {
   strict,
   type StrictPresetOptions,
-} from "https://deno.land/x/fff@v1.1.0/src/utils/presets/strict.ts";
+} from "https://deno.land/x/fff@v1.2.0-alpha.0/src/utils/presets/strict.ts";
