3ecff4ac5d28de56650a4fb81318a304586dc48d
Oscar Otero <oom@oscarotero.com>
2024-01-17 14:49:18 +0100

    updated deps

--- deps/vento.ts
+++ deps/vento.ts
@@ -1,5 +1,5 @@
-export { default as engine } from "https://deno.land/x/vento@v0.10.1/mod.ts";
-export { FileLoader } from "https://deno.land/x/vento@v0.10.1/src/loader.ts";
+export { default as engine } from "https://deno.land/x/vento@v0.10.2/mod.ts";
+export { FileLoader } from "https://deno.land/x/vento@v0.10.2/src/loader.ts";
 
-export type { Environment } from "https://deno.land/x/vento@v0.10.1/src/environment.ts";
-export type { Token } from "https://deno.land/x/vento@v0.10.1/src/tokenizer.ts";
+export type { Environment } from "https://deno.land/x/vento@v0.10.2/src/environment.ts";
+export type { Token } from "https://deno.land/x/vento@v0.10.2/src/tokenizer.ts";
