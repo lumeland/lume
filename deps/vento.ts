27c7bfc154683dcf541025c99f9171f136b116b0
Oscar Otero <oom@oscarotero.com>
2024-01-17 19:17:28 +0100

    fix vento imports

--- deps/vento.ts
+++ deps/vento.ts
@@ -1,5 +1,5 @@
-export { default as engine } from "../../../vento/mod.ts";
-export { FileLoader } from "../../../vento/src/loader.ts";
+export { default as engine } from "https://deno.land/x/vento@v0.10.2/mod.ts";
+export { FileLoader } from "https://deno.land/x/vento@v0.10.2/src/loader.ts";
 
-export type { Environment } from "../../../vento/src/environment.ts";
-export type { Token } from "../../../vento/src/tokenizer.ts";
+export type { Environment } from "https://deno.land/x/vento@v0.10.2/src/environment.ts";
+export type { Token } from "https://deno.land/x/vento@v0.10.2/src/tokenizer.ts";
