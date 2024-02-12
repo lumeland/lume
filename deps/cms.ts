17759fa48334b820930afeb9762fd1056f884a2a
Oscar Otero <oom@oscarotero.com>
2024-02-13 00:05:13 +0100

    updated cms

--- deps/cms.ts
+++ deps/cms.ts
@@ -1,5 +1,5 @@
-export { default as adapter } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.4/adapters/lume.ts";
-export { default } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.4/mod.ts";
-export { default as Kv } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.4/storage/kv.ts";
-export { default as Fs } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.4/storage/fs.ts";
-export { default as GitHub } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.4/storage/github.ts";
+export { default as adapter } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.5/adapters/lume.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.5/mod.ts";
+export { default as Kv } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.5/storage/kv.ts";
+export { default as Fs } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.5/storage/fs.ts";
+export { default as GitHub } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.5/storage/github.ts";
