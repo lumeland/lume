aa5d2548527f00ab8613047ffa4b6f1432ba177e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-13 23:06:00 +0800

    feat: new plugin `robots`

---
+++ plugins/robots.ts
@@ -0,0 +1,96 @@
+import { merge } from "../core/utils/object.ts";
+import { Page } from "../core/file.ts";
+
+import type Site from "../core/site.ts";
+
+type Rule = {
+  /** User-agent */
+  userAgent?: string;
+  /** Crawl-delay */
+  crawlDelay?: string;
+  /** Disallow */
+  disallow?: string;
+  /** Disavow */
+  disavow?: string;
+  /** Allow */
+  allow?: string;
+  /** Host */
+  host?: string;
+  /** Sitemap */
+  sitemap?: string;
+  /** Clean-param */
+  cleanParam?: string;
+};
+
+const ruleSort = [
+  "userAgent",
+  "crawlDelay",
+  "disallow",
+  "disavow",
+  "allow",
+  "host",
+  "sitemap",
+  "cleanParam",
+];
+
+export interface Options {
+  /** The robots.txt file name */
+  filename: string;
+  allow?: string[];
+  disallow?: string[];
+  rules?: Rule[];
+}
+
+// Default options
+export const defaults: Options = {
+  filename: "/robots.txt",
+  allow: ["*"],
+};
+
+/** A plugin to generate a robots.txt after build */
+export default (userOptions?: Partial<Options>) => {
+  const options: Options = merge(defaults, userOptions);
+
+  return (site: Site) => {
+    site.addEventListener("beforeSave", async () => {
+      let rules: Rule[] = [];
+
+      options.allow?.forEach((userAgent) =>
+        rules.push({
+          userAgent,
+          allow: "/",
+        })
+      );
+
+      options.disallow?.forEach((userAgent) =>
+        rules.push({
+          userAgent,
+          disallow: "/",
+        })
+      );
+
+      rules.push(...(options.rules ?? []));
+
+      // Create the robots.txt page
+      const robots = Page.create({
+        url: options.filename,
+        content: rules
+          .map((rule) =>
+            Object.entries(rule)
+              .sort(([keyA], [keyB]) =>
+                ruleSort.indexOf(keyA) - ruleSort.indexOf(keyB)
+              )
+              .map(([key, value]) =>
+                `${key.charAt(0).toUpperCase()}${
+                  key.slice(1).replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()
+                }: ${value}`
+              )
+              .join("\n")
+          ).join("\n\n"),
+      });
+
+      // Add the robots.txt to pages
+      site.pages.push(robots);
+    });
+  };
+};
