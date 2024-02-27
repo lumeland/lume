4cc75599006df423a14befc06d3ed8493c645b09
Oscar Otero <oom@oscarotero.com>
2024-02-27 17:35:51 +0100

    updated cms

--- cms.ts
+++ cms.ts
@@ -1,5 +1,5 @@
-export { default as adapter } from "https://cdn.jsdelivr.net/gh/lumeland/cms@f2f54aad39568627d9cfec5552c9f9aa92b64259/adapters/lume.ts";
-export { default } from "https://cdn.jsdelivr.net/gh/lumeland/cms@f2f54aad39568627d9cfec5552c9f9aa92b64259/mod.ts";
-export { default as Kv } from "https://cdn.jsdelivr.net/gh/lumeland/cms@f2f54aad39568627d9cfec5552c9f9aa92b64259/storage/kv.ts";
-export { default as Fs } from "https://cdn.jsdelivr.net/gh/lumeland/cms@f2f54aad39568627d9cfec5552c9f9aa92b64259/storage/fs.ts";
-export { default as GitHub } from "https://cdn.jsdelivr.net/gh/lumeland/cms@f2f54aad39568627d9cfec5552c9f9aa92b64259/storage/github.ts";
+export { default as adapter } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.11/adapters/lume.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.11/mod.ts";
+export { default as Kv } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.11/storage/kv.ts";
+export { default as Fs } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.11/storage/fs.ts";
+export { default as GitHub } from "https://cdn.jsdelivr.net/gh/lumeland/cms@v0.2.11/storage/github.ts";
