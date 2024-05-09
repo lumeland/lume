02a1b491f29d1fff16b68b76c84eed33f7bb96b3
Oscar Otero <oom@oscarotero.com>
2024-05-09 14:31:16 +0200

    removed unused function

--- core/utils/lume_version.ts
+++ core/utils/lume_version.ts
@@ -1,5 +1,3 @@
-import { cyan, green, red } from "../../deps/colors.ts";
-
 /** Return the latest stable version from the deno.land/x repository */
 export async function getLatestVersion(): Promise<string> {
   const response = await fetch("https://cdn.deno.land/lume/meta/versions.json");
@@ -22,20 +20,4 @@ export function getCurrentVersion(
 ): string {
   const { pathname } = url;
   return pathname.match(/@([^/]+)/)?.[1] ?? `local (${pathname})`;
-}
-
-/** Check the compatibility with the current Deno version */
-export function checkDenoSupport(): void {
-  const minimum = "1.38.0";
-  const current = Deno.version.deno;
-
-  if (current < minimum) {
-    console.log("----------------------------------------");
-    console.error(red("Your Deno version is not compatible with Lume"));
-    console.log(`Lume needs Deno ${green(minimum)} or greater`);
-    console.log(`Your current version is ${red(current)}`);
-    console.log(`Run ${cyan("deno upgrade")} and try again`);
-    console.log("----------------------------------------");
-    Deno.exit(1);
-  }
 }
