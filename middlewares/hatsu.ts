71f299cb13247dd6d0442a56f7e42615324e0f94
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-09 14:18:37 +0800

    feat: new middleware `hatsu`

---
+++ middlewares/hatsu.ts
@@ -0,0 +1,74 @@
+import type { Middleware } from "../core/server.ts";
+
+export interface Options {
+  /**
+   * Hatsu instance URL.
+   *
+   * @default
+   * ```ts
+   * new URL('https://hatsu.local')
+   * ```
+   */
+  instance: URL;
+  /**
+   * Match routes.
+   *
+   * @default
+   * ```ts
+   * [/^\/posts\/(.+)$/]
+   * ```
+   */
+  matches?: (RegExp | string)[];
+  /**
+   * Whether to copy `/.well-known/` files.
+   * @default `undefined`
+   */
+  wellKnown?: false;
+  /**
+   * Lume Site Location.
+   * @defaultValue
+   * ```ts
+   * new URL(req.url).host
+   * ```
+   * @example
+   * ```ts
+   * const site = lume({ location: new URL('https://example.com') })
+   * const server = new Server()
+   * server.use(hatsuMiddleware({
+   *   instance: new URL('https://hatsu.local'),
+   *   location: site.options.location,
+   * }))
+   * ```
+   */
+  location?: URL;
+}
+
+export default (options: Options): Middleware => async (req, next) => {
+  const accept = req.headers.get("accept");
+  const { origin, pathname, search } = new URL(req.url);
+
+  // redirect .well-known
+  if (pathname.startsWith("/.well-known/")) {
+    return Response.redirect(
+      new URL(pathname + search, options.instance),
+    );
+  } else if (
+    // redirect application/activity+json request
+    accept?.includes("application/activity+json") &&
+    (!options.matches ||
+      (options.matches.some((match) =>
+        match instanceof RegExp
+          ? pathname.match(match)
+          : pathname.includes(match)
+      )))
+  ) {
+    return Response.redirect(
+      new URL(
+        `/posts/${origin + pathname}`,
+        options.instance,
+      ),
+    );
+  } else {
+    return await next(req);
+  }
+};
