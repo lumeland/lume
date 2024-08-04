357f9c13638ebdd08e8ca2670981dabf76cc4c44
Oscar Otero <oom@oscarotero.com>
2024-08-04 21:34:50 +0200

    new option 'source' for tailwind

--- plugins/tailwindcss.ts
+++ plugins/tailwindcss.ts
@@ -1,6 +1,7 @@
 import tailwind from "../deps/tailwindcss.ts";
 import { getExtension } from "../core/utils/path.ts";
 import { merge } from "../core/utils/object.ts";
+import textLoader from "../core/loaders/text.ts";
 
 import type { Config } from "../deps/tailwindcss.ts";
 import type Site from "../core/site.ts";
@@ -8,6 +9,11 @@ import type Site from "../core/site.ts";
 export interface Options {
   /** Extensions processed by this plugin to extract the utility classes */
   extensions?: string[];
+
+  /**
+   * Source of the content to be processed by TailwindCSS.
+   */
+  source?: "pages" | "src";
 
   /**
    * Options passed to TailwindCSS.
@@ -18,6 +24,7 @@ export interface Options {
 
 export const defaults: Options = {
   extensions: [".html"],
+  source: "pages",
 };
 
 export default function (userOptions?: Options) {
@@ -33,14 +40,43 @@ export default function (userOptions?: Options) {
       );
     }
 
-    site.process(options.extensions, (pages) => {
-      // Get the content of all HTML pages (sorted by path)
-      const content = pages.sort((a, b) => a.src.path.localeCompare(b.src.path))
-        .map((page) => ({
-          raw: page.content as string,
-          extension: getExtension(page.outputPath).substring(1),
-        }));
+    if (options.source === "pages") {
+      site.process(options.extensions, (pages) => {
+        // Get the content of all HTML pages (sorted by path)
+        const content = pages.sort((a, b) =>
+          a.src.path.localeCompare(b.src.path)
+        )
+          .map((page) => ({
+            raw: page.content as string,
+            extension: getExtension(page.outputPath).substring(1),
+          }));
+        processContent(content);
+      });
+    } else {
+      site.process("*", async () => {
+        const content: Content[] = [];
 
+        for (const [, entry] of site.fs.entries) {
+          if (entry.type !== "file") {
+            continue;
+          }
+
+          if (!options.extensions?.includes(getExtension(entry.path))) {
+            continue;
+          }
+
+          const contentString = await entry.getContent(textLoader);
+          content.push({
+            raw: contentString.content as string,
+            extension: getExtension(entry.path).substring(1),
+          });
+        }
+
+        processContent(content);
+      });
+    }
+
+    function processContent(content: Content[]) {
       // Create Tailwind plugin
       // @ts-ignore: This expression is not callable.
       const plugin = tailwind({
@@ -64,6 +100,11 @@ export default function (userOptions?: Options) {
         tailwindPlugins = runner.normalize([plugin]);
         runner.plugins = runner.plugins.concat(tailwindPlugins);
       });
-    });
+    }
   };
+}
+
+interface Content {
+  raw: string;
+  extension: string;
 }
