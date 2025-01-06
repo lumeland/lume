0cb4906c59088dd92375fdd89dfe7a62479e99fc
Oscar Otero <oom@oscarotero.com>
2025-01-06 01:39:11 +0100

    testing tailwind 4

--- tests/tailwindcss.test.ts
+++ tests/tailwindcss.test.ts
@@ -1,17 +1,14 @@
 import { assertSiteSnapshot, build, getSite } from "./utils.ts";
-import postcss from "../plugins/postcss.ts";
 import tailwindcss from "../plugins/tailwindcss.ts";
 
-Deno.test("postcss plugin", async (t) => {
+Deno.test("Tailwindcss plugin", async (t) => {
   const site = getSite({
     src: "tailwindcss",
   });
 
   site.use(tailwindcss({
-    extensions: [".html", ".js"],
+    optimize: false,
   }));
-  site.use(postcss());
-  site.loadAssets([".js"]);
 
   await build(site);
   await assertSiteSnapshot(t, site);
