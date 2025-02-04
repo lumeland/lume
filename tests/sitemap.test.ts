cb9041758395f239dc07952d25a726a4a856a6ea
Gabriel Garcia <gbrlgrct@gmail.com>
2025-02-04 20:59:37 +0100

    Sitemap plugin: Set default query to `isRedirect!=true`.

--- tests/sitemap.test.ts
+++ tests/sitemap.test.ts
@@ -2,6 +2,7 @@ import { assertSiteSnapshot, build, getSite } from "./utils.ts";
 import sitemap from "../plugins/sitemap.ts";
 import multilanguage from "../plugins/multilanguage.ts";
 import filterPages from "../plugins/filter_pages.ts";
+import redirects from "../plugins/redirects.ts";
 
 Deno.test("Sitemap plugin", async (t) => {
   const site = getSite({
@@ -45,6 +46,19 @@ Deno.test("Sitemap plugin with filter_pages plugin", async (t) => {
   site.use(filterPages({
     fn: (page) => page.data.url !== "/pages/new-name/page7/",
   }));
+
+  await build(site);
+  await assertSiteSnapshot(t, site);
+});
+
+Deno.test("Sitemap plugin with redirects plugin", async (t) => {
+  const site = getSite({
+    src: "redirects",
+    location: new URL("https://example.com/"),
+  });
+
+  site.use(sitemap());
+  site.use(redirects());
 
   await build(site);
   await assertSiteSnapshot(t, site);
