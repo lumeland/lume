651e87ac723cfbdb9d29c1e435ce6f94a4b00b95
Oscar Otero <oom@oscarotero.com>
2025-02-19 17:48:46 +0100

    updated deps

--- deps/preact.ts
+++ deps/preact.ts
@@ -1,4 +1,4 @@
-export * as preact from "npm:preact@10.26.0";
-export * as hooks from "npm:preact@10.26.0/hooks";
+export * as preact from "npm:preact@10.26.2";
+export * as hooks from "npm:preact@10.26.2/hooks";
 export { renderToString } from "npm:preact-render-to-string@6.5.13";
-export const specifier = "npm:preact@10.26.0";
+export const specifier = "npm:preact@10.26.2";
