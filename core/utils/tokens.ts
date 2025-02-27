3c732b184cc91989b47e344e13ac2bca3e0fb85f
tyhrstan <65845697+tyhrstan@users.noreply.github.com>
2025-02-27 15:32:01 +0100

    just export the cachemap

--- core/utils/tokens.ts
+++ core/utils/tokens.ts
@@ -1,24 +1,20 @@
 import { env } from "./env.ts";
 
-const tokenCache = new Map<string, string>();
+const tokens = new Map<string, string>();
 const denoAuthTokens = env<string>("DENO_AUTH_TOKENS");
 if (denoAuthTokens) {
-  const tokens = denoAuthTokens.split(";");
-  for (const token of tokens) {
+  const authTokens = denoAuthTokens.split(";");
+  for (const token of authTokens) {
     const [credentials, host] = token.split("@");
     if (credentials.includes(":")) {
       // Basic Auth
       const encodedCredentials = btoa(credentials);
-      tokenCache.set(host, `Basic ${encodedCredentials}`);
+      tokens.set(host, `Basic ${encodedCredentials}`);
     } else {
       // Bearer Token
-      tokenCache.set(host, `Bearer ${credentials}`);
+      tokens.set(host, `Bearer ${credentials}`);
     }
   }
 }
 
-export class tokens {
-  static get(host: string): string | undefined {
-    return tokenCache.get(host);
-  }
-}
+export { tokens };
