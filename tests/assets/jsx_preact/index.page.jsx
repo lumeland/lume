dc010a3e5428f52f4ebd17ccd107b9beb0eb61ed
Oscar Otero <oom@oscarotero.com>
2025-03-23 12:54:52 +0100

    updated tests

--- tests/assets/jsx_preact/index.page.jsx
+++ tests/assets/jsx_preact/index.page.jsx
@@ -1,7 +1,7 @@
 export const layout = "layout.jsx";
 export const title = "This is the title";
 
-// Export a React component
+// Export a component
 export default (
   <>
     <h1>Hello world</h1>
