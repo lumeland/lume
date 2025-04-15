5d0058e727bcd6439b59249d49a89f0ca33e0932
Oscar Otero <oom@oscarotero.com>
2025-04-15 20:21:59 +0200

    removed pinned version of lume/init

--- deps/init.ts
+++ deps/init.ts
@@ -1,21 +1,12 @@
-// const res = await fetch(
-//   `https://cdn.deno.land/lume_init/meta/versions.json`,
-// );
-// const versions = await res.json();
-// const { run } = await import(
-//   `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
-// );
-// const { default: upgrade } = await import(
-//   `https://deno.land/x/lume_init@${versions.latest}/upgrade.ts`
-// );
-
-const commit = "ec9431f67f7a4bf6def5647af09f15370d7affc3";
-
+const res = await fetch(
+  `https://cdn.deno.land/lume_init/meta/versions.json`,
+);
+const versions = await res.json();
 const { run } = await import(
-  `https://cdn.jsdelivr.net/gh/lumeland/init@${commit}/mod.ts`
+  `https://deno.land/x/lume_init@${versions.latest}/mod.ts`
 );
 const { default: upgrade } = await import(
-  `https://cdn.jsdelivr.net/gh/lumeland/init@${commit}/upgrade.ts`
+  `https://deno.land/x/lume_init@${versions.latest}/upgrade.ts`
 );
 
 export { run, upgrade };
