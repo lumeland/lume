03c99240e741db29db637130a2300fd9ea7879a2
Oscar Otero <oom@oscarotero.com>
2025-05-05 13:13:38 +0200

    updated lume-bar

--- middlewares/reload_client.js
+++ middlewares/reload_client.js
@@ -226,7 +226,7 @@ export default function liveReload(initRevision, basepath, statusCode) {
       }
 
       const { default: DebugBar } = await import(
-        "https://cdn.jsdelivr.net/gh/lumeland/bar@d24249e5adfa8c5692aa778e38920b026531d1b8/lume-bar.js"
+        "https://cdn.jsdelivr.net/gh/lumeland/bar@0c94c59161373d5a6b617c506afc0389559fa401/lume-bar.js"
       );
       debugbar = new DebugBar();
       document.body.appendChild(debugbar);
