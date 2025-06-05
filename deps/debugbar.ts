e4676b5cda1544a6211f599dc9bd475ea8e78a28
Oscar Otero <oom@oscarotero.com>
2025-06-05 09:33:29 +0200

    updated debugbar

--- deps/debugbar.ts
+++ deps/debugbar.ts
@@ -1,6 +1,6 @@
 export type {
   Collection,
   Item,
-} from "https://cdn.jsdelivr.net/gh/lumeland/bar@3cbac12d1e5cea423b0009875ba019c116214513/types.ts";
+} from "https://cdn.jsdelivr.net/gh/lumeland/bar@a06f3477cec3e2ee219ec1aedd1851011c8bb2fb/types.ts";
 export const specifier =
-  "https://cdn.jsdelivr.net/gh/lumeland/bar@3cbac12d1e5cea423b0009875ba019c116214513/lume-bar.js";
+  "https://cdn.jsdelivr.net/gh/lumeland/bar@a06f3477cec3e2ee219ec1aedd1851011c8bb2fb/lume-bar.js";
