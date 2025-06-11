44e63e95f38e97f5b3070bfbf0caabfe5831a5d6
Oscar Otero <oom@oscarotero.com>
2025-06-11 21:08:40 +0200

    updated dependencies

--- deps/tailwindcss.ts
+++ deps/tailwindcss.ts
@@ -1,4 +1,4 @@
-export { compile } from "npm:tailwindcss@4.1.8";
-export { toSourceMap } from "npm:@tailwindcss/node@4.1.8";
-export { type ChangedContent, Scanner } from "npm:@tailwindcss/oxide@4.1.8";
-export const specifier = "https://cdn.jsdelivr.net/npm/tailwindcss@4.1.8";
+export { compile } from "npm:tailwindcss@4.1.9";
+export { toSourceMap } from "npm:@tailwindcss/node@4.1.9";
+export { type ChangedContent, Scanner } from "npm:@tailwindcss/oxide@4.1.9";
+export const specifier = "https://cdn.jsdelivr.net/npm/tailwindcss@4.1.9";
--- deps/unocss.ts
+++ deps/unocss.ts
@@ -3,11 +3,11 @@ export {
   type SourceCodeTransformer,
   type UnocssPluginContext,
   type UserConfig,
-} from "npm:@unocss/core@66.1.4";
+} from "npm:@unocss/core@66.2.0";
 
-export { presetWind3 } from "npm:@unocss/preset-wind3@66.1.4";
-export { default as transformerVariantGroup } from "npm:@unocss/transformer-variant-group@66.1.4";
-export { default as transformerDirectives } from "npm:@unocss/transformer-directives@66.1.4";
+export { presetWind3 } from "npm:@unocss/preset-wind3@66.2.0";
+export { default as transformerVariantGroup } from "npm:@unocss/transformer-variant-group@66.2.0";
+export { default as transformerDirectives } from "npm:@unocss/transformer-directives@66.2.0";
 export { default as MagicString } from "npm:magic-string@0.30.17";
 
-export const resetUrl = "https://cdn.jsdelivr.net/npm/@unocss/reset@66.1.4";
+export const resetUrl = "https://cdn.jsdelivr.net/npm/@unocss/reset@66.2.0";
