7a36d32ac84f6505491e003d7d35c0ee66951e13
Oscar Otero <oom@oscarotero.com>
2025-08-06 11:35:42 +0200

    updated test for #774

--- tests/mdx.test.ts
+++ tests/mdx.test.ts
@@ -7,8 +7,20 @@ Deno.test("Build a mdx site", async (t) => {
     src: "mdx",
   });
 
+  const plugin = () => {
+    // deno-lint-ignore no-explicit-any
+    return (tree: any) => {
+      tree.children.push({
+        type: "element",
+        tagName: "div",
+        properties: { style: "background-color: orange;" },
+        children: [{ type: "text", value: "Hello, World!" }],
+      });
+    };
+  };
+
   site.use(jsx());
-  site.use(mdx());
+  site.use(mdx({ rehypePlugins: [plugin] }));
 
   await build(site);
   await assertSiteSnapshot(t, site);
