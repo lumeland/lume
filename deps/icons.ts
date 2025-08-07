b86bf95022bbdda852e80cd31d5b5dbe3f3e26d3
Oscar Otero <oom@oscarotero.com>
2025-08-07 16:52:55 +0200

    updated tests

--- deps/icons.ts
+++ deps/icons.ts
@@ -33,7 +33,7 @@ export const catalogs: Catalog[] = [
   {
     // https://lucide.dev/
     id: "lucide",
-    src: "https://cdn.jsdelivr.net/npm/lucide-static@0.536.0/icons/{name}.svg",
+    src: "https://cdn.jsdelivr.net/npm/lucide-static@0.537.0/icons/{name}.svg",
   },
   {
     // https://fonts.google.com/icons?icon.set=Material+Symbols
