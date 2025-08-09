647a651ca562b6ff2b8dcb1cba49bf21a412db89
Oscar Otero <oom@oscarotero.com>
2025-08-09 12:26:14 +0200

    Vento v2

--- deps/vento.ts
+++ deps/vento.ts
@@ -1,9 +1,13 @@
-export { default as engine } from "https://deno.land/x/vento@v1.15.2/mod.ts";
-export { default as autotrim } from "https://deno.land/x/vento@v1.15.2/plugins/auto_trim.ts";
+export { default as engine } from "https://cdn.jsdelivr.net/gh/ventojs/vento@e5f265e20a7a19916563e161fc2f2c393724ad2f/mod.ts";
+export { default as autotrim } from "https://cdn.jsdelivr.net/gh/ventojs/vento@e5f265e20a7a19916563e161fc2f2c393724ad2f/plugins/auto_trim.ts";
+export {
+  stringifyContext,
+  VentoError,
+} from "https://cdn.jsdelivr.net/gh/ventojs/vento@e5f265e20a7a19916563e161fc2f2c393724ad2f/core/errors.ts";
 
 export type {
   Environment,
+  Loader,
   Plugin,
-} from "https://deno.land/x/vento@v1.15.2/src/environment.ts";
-export type { Loader } from "https://deno.land/x/vento@v1.15.2/src/loader.ts";
-export type { Token } from "https://deno.land/x/vento@v1.15.2/src/tokenizer.ts";
+} from "https://cdn.jsdelivr.net/gh/ventojs/vento@e5f265e20a7a19916563e161fc2f2c393724ad2f/core/environment.ts";
+export type { Token } from "https://cdn.jsdelivr.net/gh/ventojs/vento@e5f265e20a7a19916563e161fc2f2c393724ad2f/core/tokenizer.ts";
