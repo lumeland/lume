f7d7fdcb6a07732bad1bdee1f1f2c982df2eb190
Oscar Otero <oom@oscarotero.com>
2025-09-09 01:19:22 +0200

    use wasm version of resvg to prevent Buffer deprecation warning

--- deps/resvg.ts
+++ deps/resvg.ts
@@ -1,4 +1,7 @@
-import { Resvg } from "npm:@resvg/resvg-js@2.6.2";
+import { initWasm, Resvg } from "npm:@resvg/resvg-wasm@2.6.2";
+
+const url = import.meta.resolve("npm:@resvg/resvg-wasm@2.6.2/index_bg.wasm");
+await initWasm(url);
 
 export function toPng(svg: string): Uint8Array {
   const resvg = new Resvg(svg, { fitTo: { mode: "original" } });
