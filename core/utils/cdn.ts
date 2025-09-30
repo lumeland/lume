9f3aebba05efb32c23d303defc691e2a1ad4311b
Oscar Otero <oom@oscarotero.com>
2025-09-30 12:17:36 +0200

    remote function

--- core/utils/cdn.ts
+++ core/utils/cdn.ts
@@ -5,18 +5,31 @@ import {
   parse,
   parseRange,
 } from "../../deps/semver.ts";
-import { globToRegExp } from "../../deps/path.ts";
+import { fromFileUrl, globToRegExp, toFileUrl } from "../../deps/path.ts";
+import { walk, type WalkOptions } from "../../deps/fs.ts";
 
 type PackageType = "npm" | "gh";
 
-export async function getFiles(
+export function getFiles(
   specifier: string,
+  patterns: string[] = ["/**"],
 ): Promise<Map<string, string>> {
-  const result = parseNpm(specifier) || parseGh(specifier);
+  if (specifier.startsWith("file:")) {
+    return getFsFiles(specifier, patterns);
+  }
+  return getCDNFiles(specifier, patterns);
+}
+
+async function getCDNFiles(
+  specifier: string,
+  patterns: string[],
+): Promise<Map<string, string>> {
+  const result = parseNpm(specifier) || parseGh(specifier) ||
+    parseJsDelivr(specifier);
   if (!result) {
     throw new Error(`Invalid specifier: ${specifier}`);
   }
-  const [type, name, range, pattern] = result;
+  const [type, name, range, path] = result;
   const version = await getVersion(type, name, range);
   if (!version) {
     throw new Error(`No matching version found for ${name}@${range}`);
@@ -28,18 +41,19 @@ export async function getFiles(
     throw new Error(`Failed to fetch package data for ${name}@${version}`);
   }
   const data = await res.json();
-  const regexp = globToRegExp(pattern, { globstar: true, extended: true });
-  const basePath = pattern.includes("*") ? pattern.split("/*")[0] : undefined;
+  const regexps = patterns.map((pattern) =>
+    globToRegExp(path + pattern, { globstar: true, extended: true })
+  );
   const fileMap = new Map<string, string>();
 
   for (const file of data.files) {
     const filename: string = file.name;
 
-    if (!regexp.test(filename)) {
+    if (!regexps.some((regexp) => regexp.test(filename))) {
       continue;
     }
 
-    const key = basePath ? filename.slice(basePath.length) : filename;
+    const key = path ? filename.slice(path.length) : filename;
     fileMap.set(
       key,
       `https://cdn.jsdelivr.net/${type}/${name}@${version}${filename}`,
@@ -49,8 +63,40 @@ export async function getFiles(
   return fileMap;
 }
 
+async function getFsFiles(
+  specifier: string,
+  patterns: string[],
+): Promise<Map<string, string>> {
+  const basePath = fromFileUrl(specifier);
+  const fileMap = new Map<string, string>();
+  const walkOptions: WalkOptions = {
+    includeDirs: false,
+    followSymlinks: false,
+    skip: [
+      /(^|\/)\.[^\/\.]/, // hidden files
+    ],
+  };
+
+  const regexps = patterns.map((pattern) =>
+    globToRegExp(pattern, { globstar: true, extended: true })
+  );
+
+  for await (const { path } of walk(basePath, walkOptions)) {
+    const filename = path.slice(basePath.length);
+    if (!regexps.some((regexp) => regexp.test(filename))) {
+      continue;
+    }
+    fileMap.set(filename, toFileUrl(path).href);
+  }
+
+  return fileMap;
+}
+
 export function isFromCdn(specifier: string): boolean {
-  return specifier.startsWith("npm:") || specifier.startsWith("gh:");
+  return specifier.startsWith("npm:") ||
+    specifier.startsWith("gh:") ||
+    specifier.startsWith("https://cdn.jsdelivr.net/npm/") ||
+    specifier.startsWith("https://cdn.jsdelivr.net/gh/");
 }
 
 export function getFile(specifier: string): string {
@@ -58,6 +104,11 @@ export function getFile(specifier: string): string {
     return specifier.replace("npm:", "https://cdn.jsdelivr.net/npm/");
   } else if (specifier.startsWith("gh:")) {
     return specifier.replace("gh:", "https://cdn.jsdelivr.net/gh/");
+  } else if (
+    specifier.startsWith("https://cdn.jsdelivr.net/npm/") ||
+    specifier.startsWith("https://cdn.jsdelivr.net/gh/")
+  ) {
+    return specifier;
   } else {
     throw new Error(`Invalid specifier: ${specifier}`);
   }
@@ -91,12 +142,12 @@ function parseNpm(
     return;
   }
 
-  const [, scope, name, , version, pattern] = match;
+  const [, scope, name, , version, path] = match;
   return [
     "npm",
     (scope || "") + name,
     version || "*",
-    pattern || "/**",
+    path || "",
   ];
 }
 
@@ -110,11 +161,29 @@ function parseGh(
     return;
   }
 
-  const [, name, , version, pattern] = match;
+  const [, name, , version, path] = match;
   return [
     "gh",
     name,
     version || "*",
-    pattern || "/**",
+    path || "",
   ];
+}
+
+function parseJsDelivr(specifier: string): [
+  PackageType,
+  string,
+  string,
+  string,
+] | undefined {
+  if (specifier.startsWith("https://cdn.jsdelivr.net/npm/")) {
+    return parseNpm(
+      specifier.replace("https://cdn.jsdelivr.net/npm/", "npm:"),
+    );
+  }
+  if (specifier.startsWith("https://cdn.jsdelivr.net/gh/")) {
+    return parseGh(
+      specifier.replace("https://cdn.jsdelivr.net/gh/", "gh:"),
+    );
+  }
 }
