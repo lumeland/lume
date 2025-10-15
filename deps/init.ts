f8a89339fbc8eb2367b55adaf611e6ef599201e5
Oscar Otero <oom@oscarotero.com>
2025-10-15 21:09:25 +0200

    fix init version

--- deps/init.ts
+++ deps/init.ts
@@ -2,7 +2,7 @@ const res = await fetch(
   "https://data.jsdelivr.com/v1/package/gh/lumeland/init",
 );
 const data = await res.json();
-const version = data.versions.unshift();
+const version = data.versions.shift();
 const { run } = await import(
   `https://cdn.jsdelivr.net/gh/lumeland/init@${version}/mod.ts`
 );
