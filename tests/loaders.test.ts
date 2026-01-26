15451445cf3a759feaf48e033f3b7c830f58fd24
Oscar Otero <oom@oscarotero.com>
2026-01-26 20:39:11 +0100

    added changes from #809. Author @ooker777

--- tests/loaders.test.ts
+++ tests/loaders.test.ts
@@ -41,7 +41,7 @@ Deno.test("ignored draft pages on dev=false", async () => {
 
   await build(site);
 
-  assertStrictEquals(site.pages.length, 8);
+  assertStrictEquals(site.pages.length, 9);
 });
 
 Deno.test("textLoader with frontmatter containing just a comment", async () => {
