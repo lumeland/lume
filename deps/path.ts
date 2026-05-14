5ad235a9fbf3c1f5ca17bd7d19de985a1c012233
Oscar Otero <oom@oscarotero.com>
2026-05-14 21:31:31 +0200

    fix formatting issues

--- deps/path.ts
+++ deps/path.ts
@@ -1,10 +1,10 @@
 export { posix, sep as SEPARATOR } from "node:path";
-export { dirname, join, relative, extname, basename } from "node:path";
+export { basename, dirname, extname, join, relative } from "node:path";
 export { globToRegExp } from "jsr:@std/path@1.1.4/glob-to-regexp";
 export {
-  pathToFileURL as toFileUrl,
   fileURLToPath as fromFileUrl,
-} from "node:url"
+  pathToFileURL as toFileUrl,
+} from "node:url";
 
 // posix.dirname
 // posix.join
