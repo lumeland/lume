0786d086ffa42a4cdab82ddbdce4add48d1cf38b
Volpeon <git@volpeon.ink>
2026-05-20 12:51:52 +0200

    [downloader plugin] Simplify

--- plugins/downloader.ts
+++ plugins/downloader.ts
@@ -30,46 +30,27 @@ export function downloader(userOptions?: Partial<Options>) {
     site.use(modifyUrls({ fn: download }));
 
     async function download(path: string, _page: Lume.Page, element?: Element) {
+      if (
+        !isUrl(path) ||
+        !options.origins.includes(new URL(path).host) ||
+        (element && !element.matches(options.selector))
+      ) {
+        return path;
+      }
+
       const [pathAndQuery, frag] = path.split("#", 2);
 
       if (!pathAndQuery) {
         return path;
       }
 
-      const fileIndex = site.files.findIndex((file) =>
-        file.data.url === pathAndQuery
-      );
-      const file = site.files[fileIndex];
+      const content = await read(path, true);
+      const filename = posix.basename(pathAndQuery);
+      const hash = await crypto.subtle.digest("SHA-1", content);
+      const hashHex = new Uint8Array(hash).toHex();
+      const url = posix.join(options.folder, `${hashHex}-${filename}`);
 
-      let url, content;
-
-      if (file) {
-        if (!file.src.entry.flags.has("remote")) {
-          return path;
-        }
-
-        site.files.splice(fileIndex, 1);
-
-        path = file.src.entry.src;
-        content = await read(path, true);
-        url = file.data.url;
-      } else {
-        if (
-          !isUrl(path) ||
-          !options.origins.includes(new URL(path).host) ||
-          (element && !element.matches(options.selector))
-        ) {
-          return path;
-        }
-
-        const res = await fetch(pathAndQuery);
-        content = await res.arrayBuffer();
-        content = new Uint8Array(content);
-        const filename = posix.basename(pathAndQuery);
-        const hash = await crypto.subtle.digest("SHA-1", content);
-        const hashHex = new Uint8Array(hash).toHex();
-        url = posix.join(options.folder, `${hashHex}-${filename}`);
-      }
+      // site.remoteFile(url, pathAndQuery);
 
       const page = await site.getOrCreatePage(url);
       page.content = content;
