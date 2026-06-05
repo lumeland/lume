e1efdeca07fc93089f9cefded65fc381b5c497cb
Oscar Otero <oom@oscarotero.com>
2026-06-05 18:41:26 +0200

    fix formatting issues

--- deps/text.ts
+++ deps/text.ts
@@ -1,2 +1,2 @@
-export { toCamelCase } from "jsr:@std/text@1.0.19/to-camel-case"
-export { toSnakeCase } from "jsr:@std/text@1.0.19/to-snake-case"
+export { toCamelCase } from "jsr:@std/text@1.0.19/to-camel-case";
+export { toSnakeCase } from "jsr:@std/text@1.0.19/to-snake-case";
